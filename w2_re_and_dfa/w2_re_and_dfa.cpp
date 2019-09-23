c2339bd3e386ceb63054ca63f986e730815ecb82
Potter <472434557@qq.com>
2019-09-23 23:30:48 +0800

    词法分析，部分

---
+++ w2_re_and_dfa/w2_re_and_dfa.cpp
@@ -0,0 +1,47 @@
+﻿// w2_re_and_dfa.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum STATE
+{
+	INIT = 0,
+	ID,
+	DIGIT,
+	GT,
+	GT_THAN
+};
+
+string tokenText = "";
+
+void InitToken(ch)
+{
+
+}
+
+void ReadOne(char ch)
+{
+	STATE newState = INIT;
+	if (isalpha(ch))
+	{
+		newState = ID;
+		tokenText += ch;
+	}
+	else if (isdigit(ch))
+	{
+		newState = DIGIT;
+		tokenText += ch;
+	}
+	else if (ch == '>')
+	{
+		newState = GT;
+		tokenText += ch;
+	}
+}
+
+int main()
+{
+    
+}
