a41771f5ba8b3e272936ab48ceafe349058acdd4
Potter <472434557@qq.com>
2019-10-17 15:20:07 +0800

    w3, updated

--- w3_basic_formula_calc/w3_basic_formula_calc.cpp
+++ w3_basic_formula_calc/w3_basic_formula_calc.cpp
@@ -7,24 +7,27 @@
 #include "SimpleLexer.h"
 using namespace std;
 
-typedef struct {
-
-}SimpleASTNode;
-
-SimpleASTNode additive();
-SimpleASTNode multicative();
-
 int main()
 {
     std::cout << "Program Calculator!\n";
 
-	string str2 = "2+3*5+3";
-
+	string str2 = "2+3*5";
 	TokenReader reader(str2);
 	reader.Print();
 
-	SimpleLexer lexer;
-	
+	SimpleLexer lexer;	
 	ASTNode* node = lexer.additive(reader);
 	dumpTree(node, "\t");
+	cout << str2 << " calc result:" << evaluate(node) << endl;
+
+
+	cout << "===============================" << endl;
+	string str3 = "2+3+4";
+	TokenReader reader3(str3);
+	reader3.Print();
+
+	SimpleLexer lexer3;
+	ASTNode* node3 = lexer3.additive(reader3);
+	dumpTree(node3, "\t");
+	cout << str3 << " calc result:" << evaluate(node3) << endl;
 }
